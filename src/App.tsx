61a1c579f0b42d135fa2c41df8584dcd7b095434
Ravazzini, Marco <marco.ravazzini@bruker.com>
2023-09-27 19:12:29 +0200

    add shadcdn and some vscode settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
     <>
       <div className="text-lg text-red-600">ciao</div>
+      <Button>Click me</Button>
     </>
   )
 }

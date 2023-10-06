24ec242dc4f4de46337ae2eede862e5f8aa9f2ce
Marco Ravazzini <marco.ravazzini@bruker.com>
2023-10-06 11:20:54 +0200

    responsive header

--- src/App.tsx
+++ src/App.tsx
@@ -6,93 +6,30 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from './components/ui/navigation-menu'
+import { Page } from './lib/types'
 
 function App() {
   return (
     <main className="mx-auto max-w-7xl flex-1 space-y-4  ">
-      <div className="bg-white px-8 py-4">
+      <div className="bg-card px-4 py-2 md:px-8 md:py-4">
         {/* Title section */}
-        <section className="mb-4 flex items-center justify-between">
-          <h2 className="text-3xl font-bold tracking-tight ">
+        <section className="relative mb-4 flex flex-wrap items-center justify-between gap-2">
+          <h2 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-4xl">
             HA Expense Manager
           </h2>
-          <NavigationMenu>
-            <NavigationMenuList className="inline-flex h-9 items-center justify-center space-x-2 rounded-lg bg-muted p-1 text-muted-foreground">
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    activeProps={{
-                      className: 'bg-[white] text-foreground shadow',
-                    }}
-                    to="/"
-                    className="inline-flex items-center justify-center whitespace-nowrap
-                  rounded-md px-3 py-1 text-sm font-medium ring-offset-background 
-                  transition-all 
-                  focus-visible:outline-none focus-visible:ring-2 
-                  focus-visible:ring-ring focus-visible:ring-offset-2 
-                  disabled:pointer-events-none disabled:opacity-50"
-                  >
-                    Dashboard
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    to="/budget"
-                    className="inline-flex items-center justify-center whitespace-nowrap
-                  rounded-md px-3 py-1 text-sm font-medium ring-offset-background 
-                  transition-all 
-                  focus-visible:outline-none focus-visible:ring-2 
-                  focus-visible:ring-ring focus-visible:ring-offset-2 
-                  disabled:pointer-events-none disabled:opacity-50"
-                    activeProps={{
-                      className: 'bg-white text-foreground shadow',
-                    }}
-                  >
-                    Budget
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    to="/transactions"
-                    className="inline-flex items-center justify-center whitespace-nowrap
-                  rounded-md px-3 py-1 text-sm font-medium ring-offset-background 
-                  transition-all 
-                  focus-visible:outline-none focus-visible:ring-2 
-                  focus-visible:ring-ring focus-visible:ring-offset-2 
-                  disabled:pointer-events-none disabled:opacity-50"
-                    activeProps={{
-                      className: 'bg-white text-foreground shadow',
-                    }}
-                  >
-                    Transactions
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link
-                    to="/calendar"
-                    className="inline-flex items-center justify-center whitespace-nowrap
-                  rounded-md px-3 py-1 text-sm font-medium ring-offset-background 
-                  transition-all 
-                  focus-visible:outline-none focus-visible:ring-2 
-                  focus-visible:ring-ring focus-visible:ring-offset-2 
-                  disabled:pointer-events-none disabled:opacity-50"
-                    activeProps={{
-                      className: 'bg-white text-foreground shadow',
-                    }}
-                  >
-                    Calendar
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          <div className="flex items-center gap-2">
+
+          {/* Nav section */}
+          <section className="order-3 md:order-2">
+            <NavigationMenu>
+              <NavigationMenuList className="inline-flex items-center justify-center space-x-2 rounded-lg bg-muted p-1 text-muted-foreground">
+                <MainNavItem path="dashboard" value="Dashboard" />
+                <MainNavItem path="budget" value="Budget" />
+                <MainNavItem path="transactions" value="Transactions" />
+                <MainNavItem path="calendar" value="Calendar" />
+              </NavigationMenuList>
+            </NavigationMenu>
+          </section>
+          <div className="order-2 flex items-center gap-2 md:order-3">
             <Link
               to="/settings"
               className="rounded-full border bg-background p-2 shadow-sm"
@@ -101,9 +38,6 @@ function App() {
             </Link>
           </div>
         </section>
-
-        {/* Nav section */}
-        <section></section>
       </div>
 
       <section className="px-8">
@@ -114,3 +48,26 @@ function App() {
 }
 
 export default App
+
+const MainNavItem = ({ path, value }: { path: Page; value: string }) => {
+  return (
+    <NavigationMenuItem>
+      <NavigationMenuLink asChild>
+        <Link
+          to={('/' + path) as Page}
+          className="inline-flex items-center justify-center whitespace-nowrap
+            rounded-md px-3 py-1 text-xs font-medium ring-offset-background 
+            transition-all focus-visible:outline-none
+            focus-visible:ring-2 focus-visible:ring-ring 
+            focus-visible:ring-offset-2 disabled:pointer-events-none 
+            disabled:opacity-50 sm:text-sm"
+          activeProps={{
+            className: 'bg-card text-foreground shadow',
+          }}
+        >
+          {value}
+        </Link>
+      </NavigationMenuLink>
+    </NavigationMenuItem>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,8 @@ module.exports = {
     },
     extend: {
       colors: {
+        positive: 'hsl(var(--positive))',
+        negative: 'hsl(var(--negative))',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
